57d936225994fc90f79dcdc3aff36991bb016851
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-19 10:45:24 -0600

    Read input file

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -12,9 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::fs::read_to_string;
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input-1.txt").expect("Unable to read input file");
+    println!("{}", input);
 }
 
 #[cfg(not(tarpaulin_include))]
