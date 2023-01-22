2a1d02389189e8b94f0fe49c4f4992cc90de9af5
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-22 11:18:29 -0600

    Test count_overlaps

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -29,4 +29,20 @@ fn count_overlaps(input: &str) -> u32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_count_overlaps() {
+        assert_eq!(
+            2,
+            count_overlaps(
+                "2-4,6-8
+                2-3,4-5
+                5-7,7-9
+                2-8,3-7
+                6-6,4-6
+                2-6,4-8
+                "
+            )
+        );
+    }
 }
