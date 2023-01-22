25131f9a43060295538920e26bbf716586adc0e6
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-22 11:25:06 -0600

    Test count_intersections

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -65,4 +65,20 @@ mod tests {
             )
         );
     }
+
+    #[test]
+    fn test_count_intersections() {
+        assert_eq!(
+            4,
+            count_intersection(
+                "2-4,6-8
+                2-3,4-5
+                5-7,7-9
+                2-8,3-7
+                6-6,4-6
+                2-6,4-8
+                "
+            )
+        );
+    }
 }
