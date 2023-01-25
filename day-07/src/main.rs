4c97085934b63c82ce6904dfe3ce2f77881983a5
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-25 10:56:15 -0600

    Test build_file_system

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -82,4 +82,35 @@ mod tests {
         };
         assert_eq!(expected, FileSystem::new());
     }
+
+    #[test]
+    fn test_build_file_system() {
+        let input = "$ cd /
+        $ ls
+        dir a
+        14848514 b.txt
+        8504156 c.dat
+        dir d
+        $ cd a
+        $ ls
+        dir e
+        29116 f
+        2557 g
+        62596 h.lst
+        $ cd e
+        $ ls
+        584 i
+        $ cd ..
+        $ cd ..
+        $ cd d
+        $ ls
+        4060174 j
+        8033020 d.log
+        5626152 d.ext
+        7214296 k
+
+        ";
+        let file_system = build_file_system(input);
+        assert_eq!(13, file_system.nodes.len());
+    }
 }
