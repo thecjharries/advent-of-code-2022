6a65b55161062b91f084e0561b58d0c4b0d4d0a3
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-25 10:47:30 -0600

    Create item for item type

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -14,6 +14,12 @@
 
 use std::fs::read_to_string;
 
+#[derive(Debug, PartialEq)]
+enum ItemType {
+    File(u32),
+    Directory,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     let input = read_to_string("input.txt").expect("Unable to read input file");
