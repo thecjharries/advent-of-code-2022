e67357df9487ce6d08a39ba717abe4e203598927
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-27 09:01:23 -0600

    Create Movement to be read from str

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -14,6 +14,7 @@
 
 use std::collections::BTreeSet;
 use std::fs::read_to_string;
+use std::str::FromStr;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Point {
@@ -30,6 +31,29 @@ impl Default for Point {
 impl Point {
     fn new(x: i32, y: i32) -> Point {
         Point { x, y }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Movement {
+    x: i32,
+    y: i32,
+}
+
+impl FromStr for Movement {
+    type Err = ();
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let items = input.trim().split_whitespace().collect::<Vec<&str>>();
+        let direction = items[0].chars().next().unwrap();
+        let magnitude = items[1].parse::<i32>().unwrap();
+        let (x, y) = match direction {
+            'U' => (0, magnitude),
+            'D' => (0, -magnitude),
+            'L' => (-magnitude, 0),
+            'R' => (magnitude, 0),
+            _ => (0, 0),
+        };
+        Ok(Movement { x, y })
     }
 }
 
@@ -74,6 +98,24 @@ mod tests {
     fn test_new_point() {
         let expected = Point { x: 1, y: 2 };
         assert_eq!(expected, Point::new(1, 2));
+    }
+
+    #[test]
+    fn test_movement_from_str() {
+        let mut expected = Movement { x: 0, y: 1 };
+        assert_eq!(expected, "U 1".parse::<Movement>().unwrap());
+        assert_eq!(
+            expected,
+            Movement::from_str("U 1").expect("Unable to parse movement")
+        );
+        expected = Movement { x: 0, y: -1 };
+        assert_eq!(expected, "D 1".parse::<Movement>().unwrap());
+        expected = Movement { x: -1, y: 0 };
+        assert_eq!(expected, "L 1".parse::<Movement>().unwrap());
+        expected = Movement { x: 1, y: 0 };
+        assert_eq!(expected, "R 1".parse::<Movement>().unwrap());
+        expected = Movement { x: 0, y: 0 };
+        assert_eq!(expected, "X 1".parse::<Movement>().unwrap());
     }
 
     #[test]
