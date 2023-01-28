6dcb769043baa5c0491d77e00b27bf1f92538c65
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-28 09:11:10 -0600

    Implement Program.run

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -81,11 +81,14 @@ impl Program {
             Some(action) => action,
             None => return,
         };
+        self.cycles += 1;
+        if self.signal_checks.contains(&(self.cycles as usize)) {
+            self.signal_strength += self.x * self.cycles as i32;
+        }
         let cycle_time = match action {
             Action::Noop(_) => CycleTime::Noop,
             Action::Addx(_) => CycleTime::Addx,
         };
-        self.cycles += 1;
         if self.cycles == self.previous_action_cycle + cycle_time as u32 {
             self.previous_action_cycle = self.cycles;
             match action {
@@ -95,10 +98,6 @@ impl Program {
         } else {
             self.actions.push(action);
         }
-        if self.signal_checks.contains(&(self.cycles as usize)) {
-            self.signal_strength += self.x * self.cycles as i32;
-        }
-        println!("{}: {}", self.cycles, self.x);
     }
 
     fn run(&mut self) {
@@ -207,156 +206,6 @@ mod tests {
     #[test]
     fn test_program_run() {
         let mut program = Program::default();
-        // program.parse_actions(
-        //     "addx 15
-        //     addx -11
-        //     addx 6
-        //     addx -3
-        //     addx 5
-        //     addx -1
-        //     addx -8
-        //     addx 13
-        //     addx 4
-        //     noop
-        //     addx -1
-        //     addx 5
-        //     addx -1
-        //     addx 5
-        //     addx -1
-        //     addx 5
-        //     addx -1
-        //     addx 5
-        //     addx -1
-        //     addx -35
-        //     addx 1
-        //     addx 24
-        //     addx -19
-        //     addx 1
-        //     addx 16
-        //     addx -11
-        //     noop
-        //     noop
-        //     addx 21
-        //     addx -15
-        //     noop
-        //     noop
-        //     addx -3
-        //     addx 9
-        //     addx 1
-        //     addx -3
-        //     addx 8
-        //     addx 1
-        //     addx 5
-        //     noop
-        //     noop
-        //     noop
-        //     noop
-        //     noop
-        //     addx -36
-        //     noop
-        //     addx 1
-        //     addx 7
-        //     noop
-        //     noop
-        //     noop
-        //     addx 2
-        //     addx 6
-        //     noop
-        //     noop
-        //     noop
-        //     noop
-        //     noop
-        //     addx 1
-        //     noop
-        //     noop
-        //     addx 7
-        //     addx 1
-        //     noop
-        //     addx -13
-        //     addx 13
-        //     addx 7
-        //     noop
-        //     addx 1
-        //     addx -33
-        //     noop
-        //     noop
-        //     noop
-        //     addx 2
-        //     noop
-        //     noop
-        //     noop
-        //     addx 8
-        //     noop
-        //     addx -1
-        //     addx 2
-        //     addx 1
-        //     noop
-        //     addx 17
-        //     addx -9
-        //     addx 1
-        //     addx 1
-        //     addx -3
-        //     addx 11
-        //     noop
-        //     noop
-        //     addx 1
-        //     noop
-        //     addx 1
-        //     noop
-        //     noop
-        //     addx -13
-        //     addx -19
-        //     addx 1
-        //     addx 3
-        //     addx 26
-        //     addx -30
-        //     addx 12
-        //     addx -1
-        //     addx 3
-        //     addx 1
-        //     noop
-        //     noop
-        //     noop
-        //     addx -9
-        //     addx 18
-        //     addx 1
-        //     addx 2
-        //     noop
-        //     noop
-        //     addx 9
-        //     noop
-        //     noop
-        //     noop
-        //     addx -1
-        //     addx 2
-        //     addx -37
-        //     addx 1
-        //     addx 3
-        //     noop
-        //     addx 15
-        //     addx -21
-        //     addx 22
-        //     addx -6
-        //     addx 1
-        //     noop
-        //     addx 2
-        //     addx 1
-        //     noop
-        //     addx -10
-        //     noop
-        //     noop
-        //     addx 20
-        //     addx 1
-        //     addx 2
-        //     addx 2
-        //     addx -6
-        //     addx -11
-        //     noop
-        //     noop
-        //     noop
-
-        //     ",
-        // );
         program.parse_actions(
             "addx 15
             addx -11
@@ -371,11 +220,146 @@ mod tests {
             addx -1
             addx 5
             addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx 5
+            addx -1
+            addx -35
+            addx 1
+            addx 24
+            addx -19
+            addx 1
+            addx 16
+            addx -11
+            noop
+            noop
+            addx 21
+            addx -15
+            noop
+            noop
+            addx -3
+            addx 9
+            addx 1
+            addx -3
+            addx 8
+            addx 1
+            addx 5
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx -36
+            noop
+            addx 1
+            addx 7
+            noop
+            noop
+            noop
+            addx 2
+            addx 6
+            noop
+            noop
+            noop
+            noop
+            noop
+            addx 1
+            noop
+            noop
+            addx 7
+            addx 1
+            noop
+            addx -13
+            addx 13
+            addx 7
+            noop
+            addx 1
+            addx -33
+            noop
+            noop
+            noop
+            addx 2
+            noop
+            noop
+            noop
+            addx 8
+            noop
+            addx -1
+            addx 2
+            addx 1
+            noop
+            addx 17
+            addx -9
+            addx 1
+            addx 1
+            addx -3
+            addx 11
+            noop
+            noop
+            addx 1
+            noop
+            addx 1
+            noop
+            noop
+            addx -13
+            addx -19
+            addx 1
+            addx 3
+            addx 26
+            addx -30
+            addx 12
+            addx -1
+            addx 3
+            addx 1
+            noop
+            noop
+            noop
+            addx -9
+            addx 18
+            addx 1
+            addx 2
+            noop
+            noop
+            addx 9
+            noop
+            noop
+            noop
+            addx -1
+            addx 2
+            addx -37
+            addx 1
+            addx 3
+            noop
+            addx 15
+            addx -21
+            addx 22
+            addx -6
+            addx 1
+            noop
+            addx 2
+            addx 1
+            noop
+            addx -10
+            noop
+            noop
+            addx 20
+            addx 1
+            addx 2
+            addx 2
+            addx -6
+            addx -11
+            noop
+            noop
+            noop
 
             ",
         );
+        for action in program.actions.iter().rev() {
+            println!("{:?}", action);
+        }
         program.run();
-        assert_eq!(420, program.signal_strength);
-        // assert_eq!(13140, program.signal_strength);
+        assert_eq!(13140, program.signal_strength);
     }
 }
